cb6a8d6c3b63a7c6c3958969c2331bce7918c1b8
Kin Chang <kinwo.chang@sjsu.edu>
2021-11-30 16:56:05 -0800

    adjust points shadow

--- cube/cube.py
+++ cube/cube.py
@@ -303,26 +303,29 @@ def draw_shadow(display, shadow, color):
 
 color_brown = color565(45, 82, 160)
 color_green = color565(0, 200, 0)
+color_shadow = color565(105,105,105)
 
-p1 = (25,-25,75)
-p2 = (25,25,75)
-p3 = (25,25,25)
-p4 = (25,-25,25)
-p5 = (-25,-25,75)
-p6 = (-25,25,75)
-p7 = (-25,25,25)
+p1 = (25,-25,100)
+p2 = (25,25,100)
+p3 = (25,25,50)
+p4 = (25,-25,50)
+p5 = (-25,-25,100)
+p6 = (-25,25,100)
+p7 = (-25,25,50)
 cube_w = [p1, p2, p3, p4, p5, p6, p7]
 D = 100
-pv = (100,75,100)
+pv = (100,75,150)
 pl = (0, 0, 320)
+# pl = (60, -60, 320)
 cube_v = [ vector_helper.world_to_viewer_transform(pw, pv) for pw in cube_w ]
 cube_p = [ vector_helper.perspective_projection(pv, D) for pv in cube_v ]
 cube_phy = [ vector_helper.virtual_to_physical_coordinate((128,160),pp) for pp in cube_p ]
 
-draw_cube(display, cube_phy, color_green)
 cube_s = [p1, p2, p6, p5]
 shadow_w = [ vector_helper.get_shadow_on_xy_plane(pc, pl) for pc in cube_s ]
 shadow_v = [ vector_helper.world_to_viewer_transform(pw, pv) for pw in shadow_w ]
 shadow_p = [ vector_helper.perspective_projection(pv, D) for pv in shadow_v ]
 shadow_phy = [ vector_helper.virtual_to_physical_coordinate((128,160),pp) for pp in shadow_p ]
-draw_shadow(display, shadow_phy, color_brown)
+shadow_phy = [(int(p[0]), int(p[1])) for p in shadow_phy]
+draw_shadow(display, shadow_phy, color_shadow)
+draw_cube(display, cube_phy, color_green)
